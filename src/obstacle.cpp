75f42b090453cc05c6d902daa4b327930b94b7e3
Harikeshop <>
2025-06-28 20:52:31 +0530

    initial

---
+++ src/obstacle.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+ObstacleAvoider::ObstacleAvoider(int trigPin, int echoPin, float maxDistance)
+    : _trigPin(trigPin), _echoPin(echoPin), _maxDistance(maxDistance)
+{
+}
+
+void ObstacleAvoider::begin()
+{
+    pinMode(_trigPin, OUTPUT);
+    pinMode(_echoPin, INPUT);
+    digitalWrite(_trigPin, LOW);
+    delayMicroseconds(30);
+}
+
+float ObstacleAvoider::getDistance()
+{
+}
---
+++ src/obstacle.h
@@ -0,0 +1,22 @@
+#ifndef _OBSTACLE_H_
+#define _OBSTACLE_H_
+
+#include <Arduino.h>
+
+class ObstacleAvoider
+{
+public:
+    ObstacleAvoider(int trigPin, int echoPin, float maxDistance = 20.0);
+    void begin();
+    bool isObstacleDetected();
+    float getDistance();
+
+private:
+    int _trigPin;
+    int _echoPin;
+    long _duration;
+    float _distance;
+    float _maxDistance;
+};
+
+#endif
