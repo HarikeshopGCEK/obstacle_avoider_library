572ff89e47fdfec465e320905adea9a39b75ecc8
isro19692004geck <anamikapp2004@gmail.com>
2025-06-29 23:05:02 +0530

    Reset

--- src/obstacle.cpp
+++ src/obstacle.cpp
@@ -1,13 +1,11 @@
 #include <Arduino.h>
-#include "obstacle.h"
-
-// Constructor
+#include <obstacle.h>
+#include <driver.h>
 ObstacleAvoider::ObstacleAvoider(int trigPin, int echoPin, float maxDistance)
     : _trigPin(trigPin), _echoPin(echoPin), _maxDistance(maxDistance)
 {
 }
 
-// Initialize sensor pins
 void ObstacleAvoider::begin()
 {
     pinMode(_trigPin, OUTPUT);
@@ -16,42 +14,8 @@ void ObstacleAvoider::begin()
     delayMicroseconds(30);
 }
 
-// Measure distance
 float ObstacleAvoider::getDistance()
 {
-    digitalWrite(_trigPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(_trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(_trigPin, LOW);
-
-    _duration = pulseIn(_echoPin, HIGH, 30000);  // Timeout 30ms
-
-    if (_duration == 0) {
-        return _maxDistance + 1;
-    }
-
-    _distance = (_duration * 0.0343) / 2.0;
-    return _distance;
+    digitalWrite();
 }
 
-// Detect obstacle
-bool ObstacleAvoider::isObstacleDetected()
-{
-    return (getDistance() <= _maxDistance);
-}
-
-// Run logic with Serial output instead of driver control
-void ObstacleAvoider::run()
-{
-    if (isObstacleDetected()) {
-        Serial.println("Obstacle Detected!");
-        Serial.println("Action: STOP");
-        delay(200);
-        Serial.println("Action: TURN LEFT");
-        delay(400);
-    } else {
-        Serial.println("Path Clear");
-        Serial.println("Action: MOVE FORWARD");
-    }
-}
